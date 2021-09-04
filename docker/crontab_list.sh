98db2bf6c0eea0ca796e39de0507a3041032fa1b
yongyuanlin <76577177+yongyuanlin@users.noreply.github.com>
2021-09-04 23:50:12 +0800

    更新

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -211,8 +211,6 @@
 #京东价格保护
 5 0 * * * node /scripts/jd_priceProtect.js >> /scripts/logs/jd_priceProtect.log 2>&1
 ##############默认注释活动##############
-# 京东试用（默认注释，请配合取关脚本使用）
-30 6 * * *  node /scripts/jd_try.js >> /scripts/logs/jd_try.log 2>&1
 # 删除优惠券(默认注释，如需要自己开启，如有误删，已删除的券可以在回收站中还原，慎用)
 #20 9 * * 6 node /scripts/jd_delCoupon.js >> /scripts/logs/jd_delCoupon.log 2>&1
 # 京东家庭号(暂不知最佳cron)
