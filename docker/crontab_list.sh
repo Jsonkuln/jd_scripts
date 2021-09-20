03d153677e3f2d016ffc4dd2579218a9a194a6cd
yongyuanlin <154554431@qq.com>
2021-09-20 09:09:04 +0800

    update

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -88,8 +88,7 @@
 30 0,14 * * * node /scripts/jd_qycl.js >> /scripts/logs/jd_qycl.log 2>&1
 #京东手机狂欢城
 0 0-18/6 * * * node /scripts/jd_carnivalcity.js >> /scripts/logs/jd_carnivalcity.log 2>&1
-#东东玩家
-0 17 * * * node /scripts/jd_ddwj.js >> /scripts/logs/jd_ddwj.log 2>&1
+
 
 ##############长期活动##############
 # 签到
