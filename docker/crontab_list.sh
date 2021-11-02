70e21f79a532873f2de19cf742651b149a28abbe
yongyuanlin <154554431@qq.com>
2021-11-02 21:26:14 +0800

    update

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -115,7 +115,7 @@
 #任务脚本
 45 * * * * node /scripts/jd_joyjd_open.js >> /scripts/logs/jd_joyjd_open.log 2>&1
 #双11特务
-8 9,13,16，19 2-8 11 * node /scripts/jd_superBrand.js >> /scripts/logs/jd_superBrand.log 2>&1
+10 9,13,16,19,20 2-8 11 * node /scripts/jd_superBrand.js >> /scripts/logs/jd_superBrand.log 2>&1
 
 ##############长期活动##############
 # 签到
