831e281f97cd77bc050d8eb65c0d009daf9dbe63
yongyuanlin <76577177+yongyuanlin@users.noreply.github.com>
2021-11-04 16:19:10 +0800

    add 电器盲盒

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -258,6 +258,8 @@
 30 0,19 * * * node /scripts/jd_xtg.js >> /scripts/logs/jd_xtg.log 2>&1
 #星店长
 5 7,14,20 * * * node /scripts/jd_xtg.js >> /scripts/logs/jd_xtg.log 2>&1
+#电器盲盒
+0 8,17 1-20 * * node /scripts/jd_dqmh.js >> /scripts/logs/jd_dqmh.log 2>&1
 
 ##############默认注释活动##############
 # 删除优惠券(默认注释，如需要自己开启，如有误删，已删除的券可以在回收站中还原，慎用)
