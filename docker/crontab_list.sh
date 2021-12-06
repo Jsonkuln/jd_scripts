64ab3a12f2de6c4af94632534f1e92a1856475a9
yongyuanlin <76577177+yongyuanlin@users.noreply.github.com>
2021-12-06 14:37:36 +0800

    add-电脑配件

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -104,7 +104,8 @@
 13 13 * * * node /scripts/jd_gold_sign.js >> /scripts/logs/jd_gold_sign.log 2>&1
 #京东签到翻牌
 8 8 * * * node /scripts/jd_sign_flop.js >> /scripts/logs/jd_sign_flop.log 2>&1
-
+#电脑配件
+15 15 * * * node /scripts/jd_dnpj.js >> /scripts/logs/jd_dnpj.log 2>&1
 ##############长期活动##############
 # 签到
 8 0,8 * * * cd /scripts && node jd_bean_sign.js >> /scripts/logs/jd_bean_sign.log 2>&1
