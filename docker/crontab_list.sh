554206a3a8163f70917c13b11fa2794080bc5130
yongyuanlin <154554431@qq.com>
2022-01-23 22:35:06 +0800

    update

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -227,6 +227,8 @@
 0 8,12 * * * node /scripts/jd_wish.js >> /scripts/logs/jd_wish.log 2>&1
 #京东保价
 1 0,12,20 * * * node /scripts/jd_priceProtect.js >> /scripts/logs/jd_priceProtect.log 2>&1
+#京东秒秒币
+10 7 * * * node /scripts/jd_ms.js >> /scripts/logs/jd_ms.log 2>&1
 ##############默认注释活动##############
 # 删除优惠券(默认注释，如需要自己开启，如有误删，已删除的券可以在回收站中还原，慎用)
 #20 9 * * 6 node /scripts/jd_delCoupon.js >> /scripts/logs/jd_delCoupon.log 2>&1
